ac673199fd84b9f5045d7ead7b594b7fd0b272c3
Swann HERRERA <swann@devloup.dev>
2021-11-23 11:57:43 +0100

    final solution

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,72 @@
-enum Tome {
-    TOME_1 = "1",
-    TOME_2 = "2",
-    TOME_3 = "3",
-    TOME_4 = "4",
-    TOME_5 = "5",
+type SagaCount = Map<number, number>
+
+const initialCountBook: SagaCount = new Map([
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+]);
+
+const discountMap: Map<number, number> = new Map([
+    [5, 0.75],
+    [4, 0.8],
+    [3, 0.9],
+    [2, 0.95],
+    [1, 1]
+])
+
+const bookPrice = 8;
+
+function hasBooks(countBooks: SagaCount): boolean {
+    return Array.from(countBooks.values()).some(countBook => countBook > 0);
 }
 
+function countCollection(countBooks: SagaCount): number {
+    return Array.from(countBooks.keys())
+        .reduce((prev, key) =>
+            countBooks.get(key) != 0 ? 1 + prev : prev, 0);
+}
 
-function getPrice(books: number[]) {
-    const count_books = {
-        Tome.TOME_1: 0,
-        Tome.TOME_2: 0,
-        Tome.TOME_3: 0,
-        Tome.TOME_4: 0,
-        Tome.TOME_5: 0,
-    };
+function computeCollectionPrice(collectionCount: number) {
+    return (discountMap.get(collectionCount) ?? 1) * bookPrice * collectionCount;
+}
+
+function groupByBooks(books: number[]) {
+    const countBooks = initialCountBook;
+
     books.map(book => {
-        count_books[Tome[book.toString()]] += 1;
+        countBooks.set(book, (countBooks.get(book) ?? 0) + 1);
     });
-    console.dir(count_books);
+    return countBooks;
 }
 
-getPrice([1, 1, 3, 4, 5]);
+function removeComputedBooks(countBooks: SagaCount) {
+    countBooks.forEach((bookCount, key, map) => {
+        if (bookCount > 0) {
+            map.set(key, bookCount - 1);
+        }
+    })
+}
+
+function getPrice(books: number[]): number {
+    let price = 0;
+    const countBooks = groupByBooks(books);
+
+    while (hasBooks(countBooks)) {
+        const collectionCount = countCollection(countBooks);
+        price += computeCollectionPrice(collectionCount);
+        removeComputedBooks(countBooks);
+    }
+
+    return price;
+}
+
+console.log(getPrice([1, 2, 1, 2]));
+console.log(getPrice([1, 1, 1, 1, 1]));
+console.log(getPrice([1, 2, 3, 4, 5]));
+console.log(getPrice([1, 1]));
+console.log(getPrice([1, 2, 3]));
+console.log(getPrice([]));
+console.log(getPrice([1]));
+console.log(getPrice([1, 1, 2]));
